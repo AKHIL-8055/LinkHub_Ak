b7977678e50fed5aa40eeaf4641b3bdfdd5c581d
AKHIL-8055 <155914858+AKHIL-8055@users.noreply.github.com>
2025-02-25 10:53:50 +0530

    Update axios.jsx

--- my-react-app/src/axios.jsx
+++ my-react-app/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://ec2-3-108-64-248.ap-south-1.compute.amazonaws.com:8080/", 
+  baseURL: "https://linkhub-back-ak.onrender.com/", 
   withCredentials: true, 
 });
 
